3b32d5fb98ab8cf5461922be270ea1bb24147181
Fernando Rogelin <nando_rogelin@hotmail.com>
2019-03-27 21:55:50 -0300

    feature(screen): connect new screen with first screen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickClickMeButton(view: View) {
+        val intent = Intent( this, Main2Activity::class.java)
+        startActivity(intent)
     }
 }
