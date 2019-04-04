9a15917229da9e22da7f1974495ed748c98560af
alu201520597 <nando_rogelin@hotmail.com>
2019-04-03 21:57:07 -0300

    feat(permission) new class to manage permissions

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,19 +1,61 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.content_main2.*
+import java.util.jar.Manifest
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val PermissionsRequestCode = 123
+    private lateinit var managePermissions: ManagePermissions
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = listOf<String>(
+            Manifest.permission.CAMERA
+        )
+
+        managePermissions = ManagePermissions(this,list,PermissionsRequestCode)
+
+        button.setOnClickListener{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                managePermissions.checkPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        when (requestCode) {
+            PermissionsRequestCode ->{
+                val isPermissionsGranted = managePermissions
+                    .processPermissionsResult(requestCode,permissions,grantResults)
+
+                if(isPermissionsGranted){
+                    // Do the task now
+                    toast("Permissions granted.")
+                }else{
+                    toast("Permissions denied.")
+                }
+                return
+            }
+        }
     }
 
     fun clickClickMeButton(view: View) {
         val intent = Intent( this, Main2Activity::class.java)
         startActivity(intent)
     }
+}
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
