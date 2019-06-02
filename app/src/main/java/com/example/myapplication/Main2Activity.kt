6e3245364b21239c6bdab6c1257d6a2a68c8f52e
FernandoRogelin <nando_rogelin@hotmail.com>
2019-06-01 21:01:26 -0300

    feat(github): call service github

--- app/src/main/java/com/example/myapplication/Main2Activity.kt
+++ app/src/main/java/com/example/myapplication/Main2Activity.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
@@ -9,6 +11,13 @@ import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main2.*
 import kotlinx.android.synthetic.main.content_main2.*
+import android.os.AsyncTask
+import java.io.InputStreamReader
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+import android.util.JsonReader
+
+
 
 
 class Main2Activity : AppCompatActivity() {
@@ -29,7 +38,11 @@ class Main2Activity : AppCompatActivity() {
             dbHandler.addName(user)
             Toast.makeText(this, editText.text.toString() + "Added to database", Toast.LENGTH_LONG).show()
         }
+        DadosAssincronos()
 
+    }
+
+    fun buscaDadosAssincronamente() {
         button3.setOnClickListener {
             tvDisplayName.text = ""
             val dbHandler = MindOrksDBOpenHelper(this, null)
@@ -58,6 +71,57 @@ class Main2Activity : AppCompatActivity() {
         }
         constructor(userName: String) {
             this.userName = userName
+        }
+    }
+
+    fun ConnectGitHub() {
+        val githubEndpoint = URL("https://api.github.com/")
+        val myConnection = githubEndpoint.openConnection() as HttpsURLConnection
+
+        myConnection.setRequestProperty("Accept",
+            "application/vnd.github.v3+json");
+        myConnection.setRequestProperty("Contact-Me",
+            "hathibelagal@example.com");
+
+        if (myConnection.getResponseCode() == 200) {
+            val responseBody = myConnection.inputStream
+
+            val responseBodyReader = InputStreamReader(responseBody, "UTF-8")
+
+            val jsonReader = JsonReader(responseBodyReader)
+
+            jsonReader.beginObject() // Start processing the JSON object
+            while (jsonReader.hasNext()) { // Loop through all keys
+                val key = jsonReader.nextName() // Fetch the next key
+                if (key == "organization_url") { // Check if desired key
+                    // Fetch the value as a String
+                    val value = jsonReader.nextString()
+
+                    dataGitHub.append(value)
+                    dataGitHub.append("\n")
+
+                    // Do something with the value
+                    // ...
+
+                    break // Break out of the loop
+                } else {
+                    jsonReader.skipValue() // Skip values of other keys
+                }
+            }
+            jsonReader.close();
+            myConnection.disconnect();
+        } else {
+            // Error handling code goes here
+        }
+    }
+
+    inner class DadosAssincronos : AsyncTask<Void, Void, Void>() {
+
+        @SuppressLint("WrongThread")
+        override fun doInBackground(vararg params: Void): Void? {
+            buscaDadosAssincronamente()
+            ConnectGitHub()
+            return null
         }
     }
 }
